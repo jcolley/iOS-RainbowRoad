25dc9dc2f37559f8221477662eede728b3dfae09
John Colley <webjocky@gmail.com>
2017-07-18 11:55:22 -0700

    initial commit

--- iOS-RanbowRoad/ViewController.swift
+++ iOS-RanbowRoad/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  iOS-RanbowRoad
+//  AgingPeople
 //
 //  Created by John Colley on 7/18/17.
 //  Copyright © 2017 John Colley. All rights reserved.
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    let colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self as UITableViewDataSource
+        tableView.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 120.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell? =
+            tableView.dequeueReusableCell(withIdentifier: "MyCell")
+        if (cell != nil)
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.subtitle,
+                                   reuseIdentifier: "MyCell")
+        }
+        cell?.contentView.backgroundColor = colors[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
